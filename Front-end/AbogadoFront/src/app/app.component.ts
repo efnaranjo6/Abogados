7e7d52038f8394b3ecb79c6d32f2ce2c5e6cae80
efnaranjo6 <efnaranjo6@misena.edu.co>
2020-05-30 01:34:14 -0500

    modulos de abogaods

--- Front-end/AbogadoFront/src/app/app.component.ts
+++ Front-end/AbogadoFront/src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   titleA: string ;
+  title = 'AbogadoFront';
+  Abogados=[]
 
+  constructor(
+    private _http: HttpClient){}
+    
+
+  ngOnInit(){
+    this._http.get('http://127.0.0.1:8000/Persona/')
+    .subscribe((datos: any[]) => this.Abogados = datos )
+  }
 
 
   
-  title = 'AbogadoFront';
+
 }
