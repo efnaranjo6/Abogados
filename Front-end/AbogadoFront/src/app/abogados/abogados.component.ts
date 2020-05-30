3310f5ca164e07d1225107471332343c3b6efcfa
efnaranjo6 <efnaranjo6@misena.edu.co>
2020-05-30 02:24:32 -0500

    modulos con la apires en los abogados

--- Front-end/AbogadoFront/src/app/abogados/abogados.component.ts
+++ Front-end/AbogadoFront/src/app/abogados/abogados.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-abogados',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./abogados.component.css']
 })
 export class AbogadosComponent implements OnInit {
-
+  @Input("data") abogado :any
+ 
   constructor() { }
 
   ngOnInit(): void {
